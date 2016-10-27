19996f78da82d7e63a25cd8dc8c53ff47f6e4c39
zyq001 <mikezang625@gmail.com>
2016-10-27 16:54:58 +0800

    server ok

--- index.js
+++ index.js
@@ -3,32 +3,34 @@ var http = require('http');
 var numCPUs = require('os').cpus().length;
 
 function startMathJax(){
-    var mjAPI = require("MathJax-node/lib/mj-single");
+    var mjAPI = require("mathjax-node/lib/mj-page.js");
     mjAPI.config({
         MathJax: {
             SVG: {
-                font: "STIX-Web"
+                // linebreaks: { automatic: true },
+                scale: 85,
+                font: "TeX"
             },
-            tex2jax: {
-                preview: ["[math]"],
-                processEscapes: true,
-                processClass: ['math'],
-//                inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-//                displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
-                skipTags: ["script","noscript","style","textarea","pre","code"]
-            },
-            TeX: {
-                noUndefined: {disabled: true},
-                Macros: {
-                  mbox: ['{\\text{#1}}',1],
-                  mb: ['{\\mathbf{#1}}',1],
-                  mc: ['{\\mathcal{#1}}',1],
-                  mi: ['{\\mathit{#1}}',1],
-                  mr: ['{\\mathrm{#1}}',1],
-                  ms: ['{\\mathsf{#1}}',1],
-                  mt: ['{\\mathtt{#1}}',1]
-                }
-            }
+//             tex2jax: {
+//                 preview: ["[math]"],
+//                 processEscapes: true,
+//                 processClass: ['math'],
+// //                inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+// //                displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+//                 skipTags: ["script","noscript","style","textarea","pre","code"]
+//             },
+//             TeX: {
+//                 noUndefined: {disabled: true},
+//                 Macros: {
+//                   mbox: ['{\\text{#1}}',1],
+//                   mb: ['{\\mathbf{#1}}',1],
+//                   mc: ['{\\mathcal{#1}}',1],
+//                   mi: ['{\\mathit{#1}}',1],
+//                   mr: ['{\\mathrm{#1}}',1],
+//                   ms: ['{\\mathsf{#1}}',1],
+//                   mt: ['{\\mathtt{#1}}',1]
+//                 }
+//             }
         }
     });
     mjAPI.start();
@@ -41,28 +43,36 @@ function handleRequest(mjAPI, request, response){
     request.on('end', function(){
         var params = JSON.parse(str_params);
         mjAPI.typeset(params, function(result){
-            if (!result.errors) {
-                if (params.svg) {
-                    response.writeHead(200, {'Content-Type': 'image/svg+xml'});
-                    response.end(result.svg);
-                }
-                else if (params.mml) {
-                    response.writeHead(200, {'Content-Type': 'application/mathml+xml'});
-                    response.end(result.mml);
-                }
-                else if (params.png) {
-                    response.writeHead(200, {'Content-Type': 'image/png'});
-                    // The reason for slice(22) to start encoding (from str to binary)
-                    // after base64 header info--data:image/png;base64,
-                    response.end(new Buffer(result.png.slice(22), 'base64'));
-                }
-            } else {
-                response.writeHead(400, {'Content-Type': 'text/plain'});
-                response.write('Error 400: Request Failed. \n');
-                response.write(String(result.errors) + '\n');
-                response.write(str_params + '\n');
-                response.end();
-            }
+
+            response.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'});
+            response.end(result.html);
+            // if (!result.errors) {
+            //     if (params.svg) {
+            //         response.writeHead(200, {'Content-Type': 'image/svg+xml'});
+            //         response.end(result.svg);
+            //     }
+            //     else if (params.mml) {
+            //         response.writeHead(200, {'Content-Type': 'application/mathml+xml'});
+            //         response.end(result.mml);
+            //     }
+            //     else if (params.png) {
+            //         response.writeHead(200, {'Content-Type': 'image/png'});
+            //         // The reason for slice(22) to start encoding (from str to binary)
+            //         // after base64 header info--data:image/png;base64,
+            //         response.end(new Buffer(result.png.slice(22), 'base64'));
+            //     }
+            //     else if (params.html) {
+            // //
+            //         response.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'});
+            //         response.end(result.html);
+            //     }
+            // } else {
+            //     response.writeHead(400, {'Content-Type': 'text/plain'});
+            //     response.write('Error 400: Request Failed. \n');
+            //     response.write(String(result.errors) + '\n');
+            //     response.write(str_params + '\n');
+            //     response.end();
+            // }
         });
     });
 }
--- test_client.js
+++ test_client.js
@@ -1,16 +1,19 @@
 var http=require('http');
 var pdata = {
-    "format": "TeX",
-    "math": "b + y = \\sqrt{f} = \\sum_n^5 {x}",
+    "format": "TeX,MathML",
+    // "math": "b + y = \\sqrt{f} = \\sum_n^5 {x}",
     "svg":true,
     "mml":false,
+    "renderer":"SVG",
     "png":false,
+    "html":"<p align=\"left\"><b>1</b><b></b></p><p align=\"left\"><math display='inline'><semantics> <mtable columnalign='left'> <mtr> <mtd> <mtext>I=</mtext><mstyle displaystyle='true'> <mrow> <munder><mo>&#x222C;</mo> <mi>D</mi> </munder> <mrow><mi>f</mi><mo stretchy='false'>(</mo><mi>x</mi><mo>,</mo><mi>y</mi><mo stretchy='false'>)</mo><mi>d</mi><mi>&#x03C3;</mi><mo>=</mo><munder> <mrow><mi>lim</mi></mrow> <mrow><mi>d</mi><mo>&#x2192;</mo><mn>0</mn></mrow> </munder> <mstyle displaystyle='true'> <munderover><mo>&#x2211;</mo> <mrow><mi>i</mi><mo>=</mo><mn>1</mn></mrow><mi>n</mi> </munderover> <mrow><mi>f</mi><mo stretchy='false'>(</mo><msub><mi>&#x03BE;</mi><mrow><mi>i</mi><mo>,</mo></mrow> </msub> <msub><mi>&#x03B7;</mi><mi>i</mi> </msub> <mo stretchy='false'>)</mo><mi>&#x0394;</mi><msub><mi>&#x03C3;</mi><mi>i</mi> </msub> </mrow></mstyle></mrow> </mrow></mstyle><mo>,</mo><mi>&#x5176;</mi><mi>&#x4E2D;</mi><mi>d</mi><mo>=</mo><munder> <mstyle mathsize='140%' displaystyle='true'><mrow> <mi>max</mi></mrow></mstyle> <mrow><mn>1</mn><mo>&#x2264;</mo><mi>i</mi><mo>&#x2264;</mo><mi>n</mi></mrow> </munder> <mrow><mo>{</mo><mrow> <msub> <mi>d</mi> <mi>i</mi> </msub> </mrow><mo>}</mo></mrow><mo>,</mo> </mtd> </mtr> <mtr> <mtd> <msub> <mi>d</mi> <mi>i</mi> </msub><mi>&#x4E3A;</mi><mi>&#x0394;</mi><msub> <mi>&#x03C3;</mi> <mtext>i</mtext> </msub><mi>&#x7684;</mi><mi>&#x76F4;</mi><mi>&#x5F84;</mi><mi>&#xFF08;</mi><mi>i</mi><mo>=</mo><mn>1</mn><mo>,</mo><mn>2</mn><mo>,</mo><mo>&#x22EF;</mo><mi>n</mi><mo stretchy='false'>)</mo> </mtd> </mtr> </mtable> </semantics></math>test test</p>",
     "speakText": true,
     "speakRuleset": "mathspeak",
     "speakStyle": "default",
     "ex": 6,
-    "width": 1000000,
-    "linebreaks": false,
+    "width": 32,
+    "xmlns": "mml",
+    "linebreaks":  { automatic: true },
 };
 
 var datastring = JSON.stringify(pdata);
